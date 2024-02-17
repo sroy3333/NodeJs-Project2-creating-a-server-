625f265efa88fb1745fa02985f8ff0ce43ac7001
sroy3333 <sukanyaindia2222@gmail.com>
2024-02-18 00:40:17 +0530

    NodeJs-project2

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+
+
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>My Name Is Sukanya Roy</h1></body>");
+    res.write("</html>")
+    res.end();
+
+});
+
+server.listen(4000);
