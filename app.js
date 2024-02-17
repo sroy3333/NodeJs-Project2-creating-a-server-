cbf7e5ec86f423ce160a00d7c5e4cb6c84e2aedf
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-02-18 00:42:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const http = require('http');
-
-
-
-
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
@@ -10,9 +6,8 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>My First Page</title></head>");
     res.write("<body><h1>My Name Is Sukanya Roy</h1></body>");
-    res.write("</html>")
+    res.write("</html>");
     res.end();
 
 });
-
 server.listen(4000);
